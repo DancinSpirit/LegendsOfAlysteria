d57079b69f9bc3f50dbed972b7698b72ebbc7747
Maiki <maiki.moseley@gmail.com>
2021-05-16 16:12:52 -0500

    mobile stuff?

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -577,3 +577,8 @@ window.addEventListener('popstate', (event) => {
     $("#gamemaster-bottom").empty();
     loadEventId();
 })
+
+/* Mobile Stuff */
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
