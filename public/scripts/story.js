da5b7033e02b5ef3f9499286df3f63b38d7e6102
Maiki <maiki.moseley@gmail.com>
2021-05-18 11:40:02 -0500

    phone

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -9,6 +9,11 @@ $("#cutaway-image").fadeOut()
 $("#cutaway-subtitle").fadeOut();
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 window.addEventListener('popstate', (event) => {
     index=-1;
