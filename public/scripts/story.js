acaf4da918c763dab580bc2d3675208451c5153f
Maiki <maiki.moseley@gmail.com>
2022-05-10 16:21:08 -0500

    more commands

--- public/scripts/story.js
+++ public/scripts/story.js
@@ -111,26 +111,239 @@ const loadEvent = function(){
     buttons.playerBox();
 }
 
-const nextLine = function(){
+const applyFont = async function(sentText, eventId, index, font){
+    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
+    if(user.settings.pageScroll){
+        newPage(eventId);
+        createText($(`#page-${pageNum}`),eventId,index,sentText);
+    }else{
+        createText($(`#event-${eventId}`),eventId,index,sentText);
+    }
+    $(`#boxtext-${index}`).addClass(font)
+    printLine(sentText);
+}
+
+const nextLine = async function(){
     if(index<text.length-1){
         index++;
-        printLine(text[index]);
+        if(text[index].includes("[")){
+            let [command,sentText] = text[index].replace("[","").split("]");
+            switch(command){
+                case "PHILOSOPHER":
+                    applyFont(sentText, eventId, index, "philosopher");
+                    break;
+                case "CORSIVA":
+                    applyFont(sentText, eventId, index, "corsiva");
+                    break;
+                case "INDIE FLOWER":
+                    applyFont(sentText, eventId, index, "indie-flower");
+                    break;
+                case "RECEDING":
+                    text1 = sentText.split("<RECEDE>")[0];
+                    text2 = sentText.split("<RECEDE>")[1];
+                    text3 = sentText.split("<RECEDE>")[2];
+                    text4 = sentText.split("<RECEDE>")[3];
+                    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
+                    if(user.settings.pageScroll){
+                        pageCheck(eventId, index, sentText);
+                        $(`#page-${pageNum}`).append(`<p id="boxtext-${index}" style="height: ${$(`#height-check-${index}`).height()+36}px" class='boxtext'><span id="boxtext-${index}-1"></span><span class='receded-1' id="boxtext-${index}-2"></span><span class='receded-2' id="boxtext-${index}-3"></span><span class='receded-3' id="boxtext-${index}-4"></span></p>`);
+                        typing = true;
+                        await typeWriter(`#page-${pageNum}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3},text4:{id:`#boxtext-${index}-4`,text:text4}}, true);
+                        await typeWriter(`#page-${pageNum}`,"", 0, text2,index,`#boxtext-${index}-2`, 1, {text3:{id:`#boxtext-${index}-3`,text:text3},text4:{id:`#boxtext-${index}-4`,text:text4}},true);
+                        await typeWriter(`#page-${pageNum}`,"", 0, text3,index,`#boxtext-${index}-3`, 1, {text4:{id:`#boxtext-${index}-4`,text:text4}},true);
+                        await typeWriter(`#page-${pageNum}`,"", 0, text4,index,`#boxtext-${index}-4`, 1);
+                        typing = false;
+                        loadClickSignifier($(`#page-${pageNum}`));
+                    }else{
+                        $(`#event-${eventId}`).append(`<p id="boxtext-${index}" style="height: ${$(`#height-check-${index}`).height()+36}px" class='boxtext'><span id="boxtext-${index}-1"></span><span class='shadows' id="boxtext-${index}-2"></span><span id="boxtext-${index}-3"></span></p>`);
+                        typing = true;
+                        await typeWriter(`#event-${eventId}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3},text4:{id:`#boxtext-${index}-4`,text:text4}}, true);
+                        await typeWriter(`#event-${eventId}`,"", 0, text2,index,`#boxtext-${index}-2`, 1, {text3:{id:`#boxtext-${index}-3`,text:text3},text4:{id:`#boxtext-${index}-4`,text:text4}},true);
+                        await typeWriter(`#event-${eventId}`,"", 0, text3,index,`#boxtext-${index}-3`, 1, {text4:{id:`#boxtext-${index}-4`,text:text4}},true);
+                        await typeWriter(`#event-${eventId}`,"", 0, text4,index,`#boxtext-${index}-4`, 1);
+                        typing = false;
+                        loadClickSignifier($(`#event-${eventId}`));
+                    }
+                    break;
+                case "OTHER":
+                    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
+                    if(sentText.startsWith("<")){
+                        sentText = sentText.replace("<","").replace(">","");
+                       if(user.settings.pageScroll){
+                            pageCheck(eventId, index, sentText);
+                            $(`#page-${pageNum}`).append(`<p id="boxtext-${index}" class='boxtext other' style="height: ${$(`#height-check-${index}`).height()+36}px"></p>`);
+                            typing = true;
+                            await typeWriter(`#page-${pageNum}`,"", 0, sentText,index,`#boxtext-${index}`, 20);
+                            typing = false;
+                            loadClickSignifier($(`#page-${pageNum}`));
+                        }else{
+                            $(`#event-${eventId}`).append(`<p id="boxtext-${index}" class='boxtext other' style="height: ${$(`#height-check-${index}`).height()+36}px"></p>`);
+                            typing = true;
+                            await typeWriter(`#event-${eventId}`,"", 0, sentText,index,`#boxtext-${index}`, 20);
+                            typing = false;
+                            loadClickSignifier($(`#event-${eventId}`));
+                        }
+                    }else{
+                        text1 = sentText.split("<")[0] + " ";
+                        text2 = sentText.split("<")[1].split(">")[0];
+                        text3 = sentText.split(">")[1];
+                        if(user.settings.pageScroll){
+                            pageCheck(eventId, index, sentText);
+                            $(`#page-${pageNum}`).append(`<p id="boxtext-${index}" class='boxtext' style="height: ${$(`#height-check-${index}`).height()+36}px"><span id="boxtext-${index}-1"></span><span class='other' id="boxtext-${index}-2"></span><span id="boxtext-${index}-3"></span></p>`);
+                            typing = true;
+                            await typeWriter(`#page-${pageNum}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#page-${pageNum}`,"", 0, text2,index,`#boxtext-${index}-2`, 20, {text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#page-${pageNum}`,"", 0, text3,index,`#boxtext-${index}-3`, 1);
+                            typing = false;
+                            loadClickSignifier($(`#page-${pageNum}`));
+                        }else{
+                            $(`#event-${eventId}`).append(`<p id="boxtext-${index}" class='boxtext' style="height: ${$(`#height-check-${index}`).height()+36}px"><span id="boxtext-${index}-1"></span><span class='other' id="boxtext-${index}-2"></span><span id="boxtext-${index}-3"></span></p>`);
+                            typing = true;
+                            await typeWriter(`#event-${eventId}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#event-${eventId}`,"", 0, text2,index,`#boxtext-${index}-2`, 20, {text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#event-${eventId}`,"", 0, text3,index,`#boxtext-${index}-3`, 1);
+                            typing = false;
+                            loadClickSignifier($(`#event-${eventId}`));
+                        }
+                    }
+                    break;
+                case "RED":
+                    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
+                    if(sentText.startsWith("<")){
+                        sentText = sentText.replace("<","").replace(">","");
+                       if(user.settings.pageScroll){
+                            pageCheck(eventId, index, sentText);
+                            $(`#page-${pageNum}`).append(`<p id="boxtext-${index}" class='boxtext red' style="height: ${$(`#height-check-${index}`).height()+36}px"></p>`);
+                            typing = true;
+                            await typeWriter(`#page-${pageNum}`,"", 0, sentText,index,`#boxtext-${index}`, 20);
+                            typing = false;
+                            loadClickSignifier($(`#page-${pageNum}`));
+                        }else{
+                            $(`#event-${eventId}`).append(`<p id="boxtext-${index}" class='boxtext red' style="height: ${$(`#height-check-${index}`).height()+36}px"></p>`);
+                            typing = true;
+                            await typeWriter(`#event-${eventId}`,"", 0, sentText,index,`#boxtext-${index}`, 20);
+                            typing = false;
+                            loadClickSignifier($(`#event-${eventId}`));
+                        }
+                    }else{
+                        text1 = sentText.split("<")[0] + " ";
+                        text2 = sentText.split("<")[1].split(">")[0];
+                        text3 = sentText.split(">")[1];
+                        if(user.settings.pageScroll){
+                            pageCheck(eventId, index, sentText);
+                            $(`#page-${pageNum}`).append(`<p id="boxtext-${index}" class='boxtext' style="height: ${$(`#height-check-${index}`).height()+36}px"><span id="boxtext-${index}-1"></span><span class='red' id="boxtext-${index}-2"></span><span id="boxtext-${index}-3"></span></p>`);
+                            typing = true;
+                            await typeWriter(`#page-${pageNum}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#page-${pageNum}`,"", 0, text2,index,`#boxtext-${index}-2`, 20, {text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#page-${pageNum}`,"", 0, text3,index,`#boxtext-${index}-3`, 1);
+                            typing = false;
+                            loadClickSignifier($(`#page-${pageNum}`));
+                        }else{
+                            $(`#event-${eventId}`).append(`<p id="boxtext-${index}" class='boxtext' style="height: ${$(`#height-check-${index}`).height()+36}px"><span id="boxtext-${index}-1"></span><span class='red' id="boxtext-${index}-2"></span><span id="boxtext-${index}-3"></span></p>`);
+                            typing = true;
+                            await typeWriter(`#event-${eventId}`,"", 0, text1,index,`#boxtext-${index}-1`, 1, {text2:{id:`#boxtext-${index}-2`,text:text2},text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#event-${eventId}`,"", 0, text2,index,`#boxtext-${index}-2`, 20, {text3:{id:`#boxtext-${index}-3`,text:text3}});
+                            await typeWriter(`#event-${eventId}`,"", 0, text3,index,`#boxtext-${index}-3`, 1);
+                            typing = false;
+                            loadClickSignifier($(`#event-${eventId}`));
+                        }
+                    }
+                    break;
+                case "PLAYER ACTION":
+                    //Description|Title|NUMofOPTIONS|{OPTIONS WITH CHOSEN OPTION HAVING <CHOSEN>}
+                    $("#choice").empty();
+                    $("#choice").css("color","white");
+                    $("#choice-container").css("border-color","white");
+                    ctrlButton = false;
+                    enterButton = false;
+                    let numberOfOptions = parseInt(sentText.split("|")[2]);
+                    $("#choice").append(`<p class='choice-title'>${sentText.split("|")[1]}</p>`)
+                    $("#choice").append(`<p class='choice-description'>${sentText.split("|")[0]}</p>`)
+                    console.log(sentText.split("|"));
+                    for(let x=0; x<numberOfOptions; x++){
+                        if(sentText.split("|")[x+3].startsWith("<CHOSEN>")){
+                            $("#choice").append(`<div class='choice-option chosen-option'>${sentText.split("|")[x+3].replace("[CHOSEN]","")}</div>`)
+                            $(".chosen-option").on("click",function(){
+                                $("#choice-container").css("display","none");
+                                ctrlButton = true;
+                                enterButton = true;
+                            })
+                        }
+                        else
+                        $("#choice").append(`<div class='choice-option unchosen-option'>${sentText.split("|")[x+3]}</div>`)
+                    }
+                    $("#choice-container").css("display","flex");
+                    break;
+                case "REWARD":
+                    //Name|Reward Details|Character Color Id
+                    $("#choice").empty();
+                    $("#choice").css("color","black");
+                    ctrlButton = false;
+                    enterButton = false;
+                    const component = await loadComponent("rewards",false,{name:sentText.split("|")[0],rewards:sentText.split("|")[1]});
+                    const player = await loadDatabaseObject("Player",sentText.split("|")[2]);
+                    document.documentElement.style.setProperty('--light', player.colors.light);
+                    document.documentElement.style.setProperty('--dark', player.colors.dark);
+                    document.documentElement.style.setProperty('--darker', player.colors.darker);
+                    document.documentElement.style.setProperty('--highlight', player.colors.highlight);
+                    document.documentElement.style.setProperty('--background', player.colors.background);
+                    $("#choice").append(component);
+                    $("#choice").append(`<p class='continue-button'>Continue</p>`);
+                    $(".continue-button").on("click",function(){
+                        $("#choice-container").css("display","none");
+                        ctrlButton = true;
+                        enterButton = true;
+                    })
+                    $("#choice-container").css("display","flex");
+                    break;
+                case "MUSIC":
+                    await loadMusic(sentText);
+                    nextLine();
+                    break;
+                case "ONOMATOPOEIA":
+                    playSound(sentText);
+                    sentText = "<i>" + sentText + "</i>";
+                    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
+                    if(user.settings.pageScroll){
+                        pageCheck(eventId, index, sentText);
+                        createText($(`#page-${pageNum}`),eventId,index,sentText);
+                    }else{
+                        createText($(`#event-${eventId}`),eventId,index,sentText);
+                    }
+                    printLine(sentText); 
+                    break;
+                default:
+                    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${text[index]}</p>`);
+                    if(user.settings.pageScroll){
+                        pageCheck(eventId, index, text[index]);
+                        createText($(`#page-${pageNum}`),eventId,index,text[index]);
+                    }else{
+                        createText($(`#event-${eventId}`),eventId,index,text[index]);
+                    }
+                    printLine(text[index]); 
+            }
+        }else{
+            $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${text[index]}</p>`);
+            if(user.settings.pageScroll){
+                pageCheck(eventId, index, text[index]);
+                createText($(`#page-${pageNum}`),eventId,index,text[index]);
+            }else{
+                createText($(`#event-${eventId}`),eventId,index,text[index]);
+            }
+            printLine(text[index]);
+        }
     }else{
         $("#right-arrow-box").click();
     }
 }
 
 const printLine = async function(sentText){
-    $(`#event-${eventId}-height-box`).append(`<p id="height-check-${index}" class="boxtext">${sentText}</p>`);
     if(user.settings.pageScroll){
-        pageCheck(eventId, index, sentText);
-        createText($(`#page-${pageNum}`),eventId,index,sentText);
         typing = true;
         await typeWriter(`#page-${pageNum}`,"", 0, sentText, index, `#boxtext-${index}`, 1) 
         typing = false;
         loadClickSignifier($(`#page-${pageNum}`));
     }else{
-        createText($(`#event-${eventId}`),eventId,index,sentText);
         typing = true;
         await typeWriter(`#event-${eventId}`,"", 0, sentText, index, `#boxtext-${index}`, 1) 
         typing = false;
@@ -158,7 +371,6 @@ const newPage = function(eventId){
     $("#title-box-event").css("transition","0ms");
     $(`#page-${pageNum}`).css("height","auto");
     pageNum++;
-    console.log(pageNum);
     $(`#event-${eventId}`).append(`<section id="page-${pageNum}" class = 'page'></section>`)
 }
 
@@ -215,7 +427,8 @@ const typeWriter = async function(appendBox, returnString, i, txt, sentIndex, id
                         $(otherText.text4.id).text(otherText.text4.text);
                     }
                 }
-                $(`#boxtext-${sentIndex}`).css("height",`${$(`#height-check-${sentIndex}`).outerHeight(true)}px`)  
+                $(`#boxtext-${sentIndex}`).css("height",`${$(`#height-check-${sentIndex}`).outerHeight(true)}px`)
+                console.log(id + " " + txt)  
                 $(id).html(txt) 
                 clearTimeout(timeout);
                 typeInterrupt = false;
