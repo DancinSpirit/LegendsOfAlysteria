b782a2954a2bc5c21ccf3d8279b914b4b8435e30
Maiki <maiki.moseley@gmail.com>
2022-06-04 16:33:48 -0500

    gamemaster quote handling

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -353,3 +353,7 @@ window.onload = function(){
     loadStates();
 }
 
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
